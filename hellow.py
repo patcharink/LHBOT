be448f5d5c43b93b028629a43a0996bc52dddca8
PatcharinK <insom.patcharin@gmail.com>
2017-09-19 11:29:48 +0700

    index.py

--- hellow.py
+++
@@ -1,9 +0,0 @@
-import random
-while True:
-    text = input("> ")
-    a = ['HI','Hello']
-    b = ['Hello :D','Hi',':D']
-    if text in a:
-        print(random.choice(b))
-    else:
-        print("?")

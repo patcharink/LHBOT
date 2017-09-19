bb232c05f9a2dafc688bdedf74b12da0ab910702
PatcharinK <insom.patcharin@gmail.com>
2017-09-19 11:21:34 +0700

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -1,1 +1,9 @@
-print("?")
+import random
+while True:
+    text = input("> ")
+    a = ['HI','Hello']
+    b = ['Hello :D','Hi',':D']
+    if text in a:
+        print(random.choice(b))
+    else:
+        print("?")
